5339cb709ad58368ffad6226e267dea3648bbcae
PINAKIN BRAHMIN <pinakin2050@outlook.com>
2020-08-15 19:52:40 +0530

    Added new project PropertyDemo

---
+++ PropertyDemo/ConditionalProperty.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PropertyDemo
+{
+    class ConditionalProperty
+    {
+        double _Balance = 1000;
+
+        public double Balance
+        {
+            get { return _Balance; }
+            set //If the condition is satisfied then value of _Balance will be updated else it will be the same.
+            {
+                if (value < _Balance)
+                    _Balance -= value;
+            }
+        }
+
+        static void Main()
+        {
+            ConditionalProperty obj = new ConditionalProperty();
+            double withdrawalAmount = 4000;
+            obj.Balance = withdrawalAmount;
+            Console.WriteLine("Value of _Balance: " + obj.Balance);
+
+            withdrawalAmount = 500;
+            obj.Balance = withdrawalAmount;
+            Console.WriteLine("Updated value of _Balance: " + obj.Balance);
+        }
+    }
+}
---
+++ PropertyDemo/Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PropertyDemo
+{
+    class Program1
+    {
+        double _Radius = 12.34;
+        
+        //Instead of using getter and setter methods for private fields use property to combine both methods together.
+        public double Radius
+        {
+            get { return _Radius; } //Get accessor
+            set { _Radius = value; } //Set accessor. Here value is implicitly defined and its type is same as the type of property
+        }
+        static void Main()
+        {
+            Program1 p = new Program1();
+            double radius = p.Radius; //Using Get Accessor
+            p.Radius = 15.34; // Using Set Accessor
+            Console.WriteLine("Value of Local radius: " + radius);
+            Console.WriteLine("Updated value of Radius: " + p.Radius);
+        }
+    }
+}
