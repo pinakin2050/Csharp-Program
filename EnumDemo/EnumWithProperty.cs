db89aef47e9fd636db73d16bcb05f4e5e529f896
PINAKIN BRAHMIN <pinakin2050@outlook.com>
2020-08-15 17:19:34 +0530

    Added a new project EnumDemo.

---
+++ EnumDemo/EnumWithProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    public enum Day
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday
+    }
+    class EnumWithProperty
+    {
+        //Defining an auto property with default values Monday
+        //Making it static to directly use it in Main() 
+        public static Day MeetingDay { get; set; } = Day.Monday;
+
+        static void Main()
+        {
+            Console.WriteLine("Default Meeting Day: " + MeetingDay);
+            //Setting another meeting day
+            MeetingDay = Day.Tuesday;
+            Console.WriteLine("Updated Meeting Day: " + MeetingDay);
+        }
+    }
+}
---
+++ EnumDemo/TypeCastedEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    public enum Colors : byte //Supported types are: byte,short,int,long,uint,ushort,ulong and sbyte
+    {
+        Red, Blue, Green, Black, Purple
+    }
+    class TypeCastedEnum
+    {
+       static void Main()
+        {
+            Colors c = Colors.Red;
+            Console.WriteLine("My fav color is " + c);
+
+            foreach(byte i in Enum.GetValues(typeof(Colors)))
+            {
+                Console.WriteLine(i + " : " + (Colors)i);
+            }
+        }
+    }
+}
