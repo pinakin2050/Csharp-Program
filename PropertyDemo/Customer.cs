3177f51fa5438313f2aa0f6172cfbb5617504523
PINAKIN BRAHMIN <pinakin2050@outlook.com>
2020-08-17 21:56:41 +0530

    Added new classes Customer and Execute Customer.

---
+++ PropertyDemo/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PropertyDemo
+{
+    class Customer
+    {
+        public const int MinimumBalance = 500;
+        int _CustomerID = 001;
+        string _CustomerName;
+        bool _CustomerStatus;
+        double _CustomerBalance;
+
+        public int CustomerID
+        {
+            get { return _CustomerID; }
+        }
+
+        public string CustomerName
+        {
+            get { return _CustomerName; }
+            set
+            {
+                if (_CustomerStatus)
+                    _CustomerName = value;
+            }
+        }
+
+        public bool CustomerStatus
+        {
+            get { return _CustomerStatus; }
+            set { _CustomerStatus = value; }
+        }
+
+        public double CustomerBalance
+        {
+            get { return _CustomerBalance; }
+            set
+            {
+                if(_CustomerStatus)
+                {
+                    if (value > MinimumBalance)
+                        _CustomerBalance = value;
+                }
+            }
+        }
+    }
+}
---
+++ PropertyDemo/ExecuteCustomer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyDemo
+{
+    class ExecuteCustomer
+    {
+        static void Main()
+        {
+            Customer obj = new Customer();
+            obj.CustomerStatus = true;
+            obj.CustomerName = "Pinakin Brahmin";
+            obj.CustomerBalance = 7000;
+
+            Console.WriteLine("Customer Details:");
+            Console.WriteLine("Name: " + obj.CustomerName);
+            Console.WriteLine("Balance: " + obj.CustomerBalance);
+
+            //Editing CustomerID is not allowed
+            //obj.CustomerID = 002;
+
+            obj.CustomerStatus = false;
+            //Due to applied condtion the name of inactive customer will not get changed
+            obj.CustomerName = "Chris Hemsworth"; //This statement will have no effect
+            Console.WriteLine("Updated Name: " + obj.CustomerName);
+
+            obj.CustomerStatus = true;
+            //Trying to withdraw more amount than the available balance
+            //But this statement won't have any effect because of applied condition
+            obj.CustomerBalance -= 8000;
+            Console.WriteLine("Updated Amount: " + obj.CustomerBalance);
+
+            //Trying to withdraw smaller amount than the available balance
+            obj.CustomerBalance -= 3000;
+            Console.WriteLine("Updated Amount: " + obj.CustomerBalance);
+        }
+    }
+}
