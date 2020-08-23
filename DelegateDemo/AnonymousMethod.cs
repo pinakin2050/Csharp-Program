c995dbcb7c1a6440b28ff08be463c586ee65a860
PINAKIN BRAHMIN <pinakin2050@outlook.com>
2020-08-23 13:46:41 +0530

    Added new class AnonymousMethod in DelegateDemo project.

---
+++ DelegateDemo/AnonymousMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DelegateDemo
+{
+    public delegate string Greet(string name);
+    class AnonymousMethod
+    {
+        internal string Greetings(string name)
+        {
+            return "Hello " + name + " very good morning!"; 
+        }
+
+        static void Main()
+        {
+            AnonymousMethod obj = new AnonymousMethod();
+            Greet gd1 = new Greet(obj.Greetings);
+
+            //Instead of calling the small methods we can define code of block without name
+            //Which is called anonymous method
+            Greet gd2 = delegate(string name)
+            {
+                 return "Hello " + name + " very good morning!";
+            };
+
+            Console.WriteLine(gd2.Invoke("Pinakin"));
+        }
+    }
+}
